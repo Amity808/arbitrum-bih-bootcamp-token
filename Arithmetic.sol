2fbe69caff9b1d1a4b1abfdccd63cdff59826f68
Muhdsodiq Bolarinwa <105107754+Amity808@users.noreply.github.com>
2024-01-20 13:00:20 -0800

    Update Arithmetic.sol

--- Arithmetic.sol
+++ Arithmetic.sol
@@ -16,20 +16,47 @@ contract Arithmetic {
    } 
 
    
-    function getMostSignificantBit(uint256 _number) public pure returns (uint8) {
-        // Ensure the number is not zero
-        require(_number > 0, "Number must be greater than zero");
+    function getMostSignificantBit(uint256 _number) public pure returns (uint8 result) {
+        if(_number >= 2 ** 128) {
+            _number >>= 128;
+            result += 128;
 
-        // Calculate the position of the most significant bit
-        uint8 msbPosition = 0;
-        uint256 tempNumber = _number;
-
-        while (tempNumber > 1) {
-            tempNumber >>= 1;
-            msbPosition++;
         }
 
-        return msbPosition;
+        if (_number >= 2 ** 64) {
+            _number >>= 64;
+            result += 64;
+        }
+
+        if (_number >= 2 ** 32) {
+            _number >>= 32;
+            result += 32;
+        }
+
+        if (_number >= 2 ** 16) {
+            _number >>= 16;
+            result += 16;
+        }
+
+        if (_number >= 2 ** 8) {
+            _number >>= 8;
+            result += 8;
+        }
+
+        if (_number >= 2 ** 4) {
+            _number >>= 4;
+            result += 4;
+        }
+
+        if (_number >= 2 ** 2) {
+            _number >>= 2;
+            result += 2;
+        }
+
+        if (_number >= 2) {
+            result += 1;
+        }
+
     }
     
 }
